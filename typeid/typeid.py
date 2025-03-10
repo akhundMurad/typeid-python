f66753d61a72647b1675430b4503a2c8387ff4c3
Murad Akhundov <akhundov1murad@gmail.com>
2025-03-10 16:28:52 +0100

    fix: clean up whitespaces

--- typeid/typeid.py
+++ typeid/typeid.py
@@ -81,18 +81,18 @@ def from_uuid(suffix: uuid6.UUID, prefix: Optional[str] = None) -> TypeID:
 
 def get_prefix_and_suffix(string: str) -> tuple:
     parts = string.rsplit("_", 1)
-    
+
     # When there's no underscore in the string.
     if len(parts) == 1:
         if parts[0].strip() == "":
             raise InvalidTypeIDStringException(f"Invalid TypeID: {string}")
         return None, parts[0]
-    
+
     # When there is an underscore, unpack prefix and suffix.
     prefix, suffix = parts
     if prefix.strip() == "" or suffix.strip() == "":
         raise InvalidTypeIDStringException(f"Invalid TypeID: {string}")
-    
+
     return prefix, suffix
 
 
